ada43844ca5f3c775bc98fab2663f2543c54cd61
Ed Balogh <ed.balogh@gmail.com>
2026-05-05 14:37:03 -0400

    feat: export KalshiDataClient from adapters.kalshi

--- adapters/kalshi/__init__.py
+++ adapters/kalshi/__init__.py
@@ -1,10 +1,12 @@
 from adapters.kalshi.config import KalshiDataClientConfig, KalshiExecClientConfig
 from adapters.kalshi.constants import KALSHI_VENUE
+from adapters.kalshi.data import KalshiDataClient
 from adapters.kalshi.execution import KalshiExecutionClient
 from adapters.kalshi.providers import KalshiInstrumentProvider
 
 __all__ = [
     "KALSHI_VENUE",
+    "KalshiDataClient",
     "KalshiDataClientConfig",
     "KalshiExecClientConfig",
     "KalshiExecutionClient",
